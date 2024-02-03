dcb5ca4cc6a6490f1d13384100d467160462811d
Artem <selfex9496@gmail.com>
2024-02-03 18:56:15 +0700

    vercel error fix

---
+++ src/components/Icons/CloseIcon.tsx
@@ -0,0 +1,14 @@
+export const CloseIcon = () => (
+  <svg
+    width="24px"
+    height="24px"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none" stroke="currentColor"
+    strokeWidth="2" strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <line x1="18" y1="6" x2="6" y2="18"></line>
+    <line x1="6" y1="6" x2="18" y2="18"></line>
+  </svg>
+);
