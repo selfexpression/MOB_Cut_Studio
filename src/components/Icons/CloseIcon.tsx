89f714ab2d5f5b306e0646dd963dbeb6c40d76b2
Artem <selfex9496@gmail.com>
2024-02-05 18:13:07 +0700

    layout fixes

--- src/components/Icons/CloseIcon.tsx
+++ src/components/Icons/CloseIcon.tsx
@@ -7,6 +7,7 @@ export const CloseIcon = () => (
     fill="none" stroke="currentColor"
     strokeWidth="2" strokeLinecap="round"
     strokeLinejoin="round"
+    className="close-logo"
   >
     <line x1="18" y1="6" x2="6" y2="18"></line>
     <line x1="6" y1="6" x2="18" y2="18"></line>
