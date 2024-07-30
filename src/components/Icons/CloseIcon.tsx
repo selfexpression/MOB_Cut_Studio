cfdb812d6de89a9ec612f73e65e2ebc7f1969fd0
Artem <selfex9496@gmail.com>
2024-07-30 14:58:36 +0700

    fix position and color

--- src/components/Icons/CloseIcon.tsx
+++ src/components/Icons/CloseIcon.tsx
@@ -5,7 +5,7 @@ export const CloseIcon = () => (
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    stroke="black"
+    stroke="white"
     strokeWidth="2"
     strokeLinecap="round"
     strokeLinejoin="round"
